9d2e25d687e7295c965e86d4bc62a6e1a48bc602
Kern Agustin <aguskern@gmail.com>
2022-08-24 00:21:53 -0300

    subiendo archivos segundo desafio react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import Navbar from "./components/Navbar"
+function App() {
+
+  
+
+  return (
+    <div classname="App">
+      <Navbar/>
+      <h1>Hola mundo</h1>
+      <br />
+      <a href="http://google.com">ir a google</a>
+    </div>
+  );
+}
+
+export default App;
